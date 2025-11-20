10341c4723d957ee45ab1433c4c4d78a96813cf4
unknown <harsh.kumar@digitalxperts.in>
2025-11-20 22:04:40 +0530

    Update GTAG for correct placement

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,26 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+         {/* Script that must be inside <head> */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-4CKQSZF4DS"
+        ></script>
       </Head>
       <body>
+         {/* Script that must load after <body> starts */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){ dataLayer.push(arguments); }
+              gtag('js', new Date());
+              gtag('config', 'G-4CKQSZF4DS', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
         <Main />
         <NextScript />
       </body>
